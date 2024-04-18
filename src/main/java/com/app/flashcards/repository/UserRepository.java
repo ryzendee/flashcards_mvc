418a4828cca7f3095d19246aef849a5b51e5a2b8
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-18 17:12:32 +0400

    feat: Added UserRepository

---
+++ src/main/java/com/app/flashcards/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.app.flashcards.repository;
+
+import com.app.flashcards.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+}
