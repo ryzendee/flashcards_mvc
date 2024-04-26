a5039bf9f21eb96de59d634c8c50a10b7eb8f5d3
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-26 11:13:08 +0400

    feat: Added card folder repository

---
+++ src/main/java/com/app/flashcards/repository/CardFolderRepository.java
@@ -0,0 +1,11 @@
+package com.app.flashcards.repository;
+
+import com.app.flashcards.entity.CardFolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardFolderRepository extends JpaRepository<CardFolder, Long> {
+
+    Page<CardFolder> findAllByUserId(Long userId, Pageable pageable);
+}
