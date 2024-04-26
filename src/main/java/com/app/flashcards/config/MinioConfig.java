7a9154b6aa4030c934ff42f8247844c525dabd68
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-26 11:23:58 +0400

    feat: Added minio config

---
+++ src/main/java/com/app/flashcards/config/MinioConfig.java
@@ -0,0 +1,37 @@
+package com.app.flashcards.config;
+
+import io.minio.MinioClient;
+import jakarta.validation.constraints.NotBlank;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class MinioConfig {
+
+    private final String endpoint;
+    private final String bucketName;
+    private final String secretKey;
+    private final String accessKey;
+
+    public MinioConfig(
+            @NotBlank @Value("${minio.endpoint}") String endpoint,
+            @NotBlank @Value("${minio.bucket-name}") String bucketName,
+            @NotBlank @Value("${minio.secret-key}") String secretKey,
+            @NotBlank @Value("${minio.access-key}") String accessKey) {
+        this.endpoint = endpoint;
+        this.bucketName = bucketName;
+        this.secretKey = secretKey;
+        this.accessKey = accessKey;
+    }
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+}
