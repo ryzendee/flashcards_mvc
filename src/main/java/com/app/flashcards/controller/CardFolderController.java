6716acac4a0546bfcd9ced0521e2c8ddcbcb1d37
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-26 11:09:01 +0400

    refactor: Added constructor, annotations and text column type to imageUrl

---
+++ src/main/java/com/app/flashcards/controller/CardFolderController.java
@@ -0,0 +1,61 @@
+package com.app.flashcards.controller;
+
+import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
+import com.app.flashcards.dto.response.CardFolderDtoResponse;
+import com.app.flashcards.facade.cardfolder.CardFolderServiceFacade;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class CardFolderController {
+    private static final String DEFAULT_PAGE = "0";
+    private static final String DEFAULT_SIZE = "10";
+
+    private final CardFolderServiceFacade folderServiceFacade;
+
+    @GetMapping("/folders")
+    public String getCardFoldersView(@SessionAttribute Long userId,
+                                          @RequestParam(defaultValue = DEFAULT_PAGE) int page,
+                                          @RequestParam(defaultValue = DEFAULT_SIZE) int size,
+                                          Model model) {
+
+        Page<CardFolderDtoResponse> foldersPage = folderServiceFacade.getCardFolderPageByUserId(userId, page, size);
+
+        model.addAttribute("foldersPageList", foldersPage);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", foldersPage.getTotalPages());
+
+        return "cardfolder/cardfolder-main-view";
+    }
+
+    @GetMapping("/folders-add")
+    public String getAddFolderView(Model model) {
+        CardFolderCreateDtoRequest folderDtoRequest = new CardFolderCreateDtoRequest();
+
+        model.addAttribute("cardFolder", folderDtoRequest);
+
+        return "cardfolder/cardfolder-add-view";
+    }
+
+    @PostMapping("/folders-add")
+    public String saveCreatedFolder(@Valid @ModelAttribute CardFolderCreateDtoRequest cardFolderCreateDtoRequest,
+                               @SessionAttribute Long userId,
+                               BindingResult bindingResult,
+                               Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "cardfolder/cardfolder-add-view";
+        }
+
+        folderServiceFacade.createCardFolder(userId, cardFolderCreateDtoRequest);
+
+        return "redirect:/flashcard-folders";
+    }
+}
--- src/main/java/com/app/flashcards/entity/CardFolder.java
+++ src/main/java/com/app/flashcards/entity/CardFolder.java
@@ -1,17 +1,37 @@
 package com.app.flashcards.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 @Entity
+@Table(
+        name = "card_folders"
+)
 public class CardFolder {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
+
     private String description;
+
+    @Column(columnDefinition = "TEXT")
     private String imageUrl;
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    public CardFolder(String name, String description, String imageUrl, User user) {
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.user = user;
+    }
 }
