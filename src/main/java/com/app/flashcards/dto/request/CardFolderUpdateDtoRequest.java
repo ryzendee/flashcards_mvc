8925ba819a291c81e9f837dc4ec6215fe4e6be30
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-04-29 11:45:50 +0400

    feat: Added request dto for update folder

---
+++ src/main/java/com/app/flashcards/dto/request/CardFolderUpdateDtoRequest.java
@@ -0,0 +1,17 @@
+package com.app.flashcards.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CardFolderUpdateDtoRequest {
+    private Long id;
+    @NotBlank(message = "Folder name must not be blank or empty.")
+    private String name;
+    private String description;
+    private MultipartFile image;
+}
