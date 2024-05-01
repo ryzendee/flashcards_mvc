9e4da38410f8070b02c9352d69fb3d1ba111ce26
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-01 14:44:02 +0400

    refactor: Renamed description to definition

--- src/main/java/com/app/flashcards/dto/response/FlashcardDtoResponse.java
+++ src/main/java/com/app/flashcards/dto/response/FlashcardDtoResponse.java
@@ -1,9 +1,19 @@
 package com.app.flashcards.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FlashcardDtoResponse {
 
     private Long id;
+    private Long folderId;
     private String name;
-    private String description;
+    private String definition;
     private String imageUrl;
 }
