92a695a5b486f9f6b168d313a161a3a8997aefd7
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-06 11:54:54 +0400

    refactor: Added exception message in loadByUsername method

--- src/main/java/com/app/flashcards/service/user/UserDetailsServiceImpl.java
+++ src/main/java/com/app/flashcards/service/user/UserDetailsServiceImpl.java
@@ -18,6 +18,6 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException(""));
+                .orElseThrow(() -> new UsernameNotFoundException("User with this username not found! Username: " + username));
     }
 }
