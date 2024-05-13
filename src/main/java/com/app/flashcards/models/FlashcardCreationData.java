4d1a7fec9941b267e8e05d52da2842dad405da9d
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 10:51:15 +0500

    refactor: Deleted CardFolder field

--- src/main/java/com/app/flashcards/models/FlashcardCreationData.java
+++ src/main/java/com/app/flashcards/models/FlashcardCreationData.java
@@ -1,6 +1,5 @@
 package com.app.flashcards.models;
 
-import com.app.flashcards.entity.CardFolder;
 
-public record FlashcardCreationData(CardFolder cardFolder, String imageUrl) {
+public record FlashcardCreationData(String imageUrl) {
 }
