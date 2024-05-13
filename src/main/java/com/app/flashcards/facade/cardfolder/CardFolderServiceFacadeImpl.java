384f1ae9b471faae086e9d300b3ecb1738b2c695
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 11:04:45 +0500

    fix: Deleted getById using UserService due to transaction atomicity issues

--- src/main/java/com/app/flashcards/facade/cardfolder/CardFolderServiceFacadeImpl.java
+++ src/main/java/com/app/flashcards/facade/cardfolder/CardFolderServiceFacadeImpl.java
@@ -3,8 +3,6 @@ package com.app.flashcards.facade.cardfolder;
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.dto.request.CardFolderUpdateDtoRequest;
 import com.app.flashcards.dto.response.CardFolderDtoResponse;
-import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.entity.User;
 import com.app.flashcards.enums.ImagePath;
 import com.app.flashcards.mapper.cardfolder.CardFolderMapper;
 import com.app.flashcards.models.CardFolderCreationData;
@@ -12,7 +10,6 @@ import com.app.flashcards.models.CardFolderUpdateData;
 import com.app.flashcards.models.ImageData;
 import com.app.flashcards.service.cardfolder.CardFolderService;
 import com.app.flashcards.service.image.ImageService;
-import com.app.flashcards.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -23,18 +20,14 @@ import org.springframework.web.multipart.MultipartFile;
 public class CardFolderServiceFacadeImpl implements CardFolderServiceFacade {
     private final CardFolderService cardFolderService;
     private final ImageService imageService;
-    private final UserService userService;
     private final CardFolderMapper cardFolderMapper;
 
     @Override
     public void createCardFolder(Long userId, CardFolderCreateDtoRequest createRequest) {
-
         ImageData imageData = createImageData(userId, createRequest.getImage());
         String imageUrl = imageService.uploadImage(imageData);
-        User user = userService.getUserById(userId);
-        CardFolderCreationData data = new CardFolderCreationData(user, imageUrl);
-
-        CardFolder cardFolder = cardFolderService.createCardFolder(createRequest, data);
+        CardFolderCreationData data = new CardFolderCreationData(userId, imageUrl);
+        cardFolderService.createCardFolder(createRequest, data);
     }
 
     @Override
@@ -48,7 +41,7 @@ public class CardFolderServiceFacadeImpl implements CardFolderServiceFacade {
         ImageData imageData = createImageData(userId, updateRequest.getImage());
         String imageUrl = imageService.uploadImage(imageData);
         CardFolderUpdateData data = new CardFolderUpdateData(imageUrl);
-        CardFolder updatedFolder = cardFolderService.update(updateRequest, data);
+        cardFolderService.update(updateRequest, data);
     }
 
     @Override
