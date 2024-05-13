fe81a307bfcebf3681e886e93e990415fba7e7c4
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 16:32:08 +0500

    refactor: Deleted CreationData from arguments

--- src/main/java/com/app/flashcards/factory/cardfolder/CardFolderFactory.java
+++ src/main/java/com/app/flashcards/factory/cardfolder/CardFolderFactory.java
@@ -2,10 +2,8 @@ package com.app.flashcards.factory.cardfolder;
 
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.entity.User;
-import com.app.flashcards.models.CardFolderCreationData;
 
 public interface CardFolderFactory {
 
-    CardFolder createFromRequest(CardFolderCreateDtoRequest createRequest, CardFolderCreationData data);
+    CardFolder createFromRequest(CardFolderCreateDtoRequest createRequest);
 }
--- src/main/java/com/app/flashcards/factory/cardfolder/CardFolderFactoryImpl.java
+++ src/main/java/com/app/flashcards/factory/cardfolder/CardFolderFactoryImpl.java
@@ -2,14 +2,13 @@ package com.app.flashcards.factory.cardfolder;
 
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.models.CardFolderCreationData;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CardFolderFactoryImpl implements CardFolderFactory {
 
     @Override
-    public CardFolder createFromRequest(CardFolderCreateDtoRequest createRequest, CardFolderCreationData data) {
-        return new CardFolder(createRequest.getName(), createRequest.getDescription(), data.imageUrl(), data.user());
+    public CardFolder createFromRequest(CardFolderCreateDtoRequest createRequest) {
+        return new CardFolder(createRequest.getName(), createRequest.getDescription());
     }
 }
