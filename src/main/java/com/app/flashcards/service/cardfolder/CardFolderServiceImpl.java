56f6f97a56664df0163c8ac4758303e6bd6c455f
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 11:12:25 +0500

    fix: Added transcational annotations to CardFolderService

--- src/main/java/com/app/flashcards/service/cardfolder/CardFolderServiceImpl.java
+++ src/main/java/com/app/flashcards/service/cardfolder/CardFolderServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -27,6 +28,7 @@ public class CardFolderServiceImpl implements CardFolderService {
     private final CardFolderFactory cardFolderFactory;
 
 
+    @Transactional
     @Override
     public CardFolder createCardFolder(CardFolderCreateDtoRequest createRequest, CardFolderCreationData data) {
         return userRepository.findById(data.userId())
@@ -40,6 +42,7 @@ public class CardFolderServiceImpl implements CardFolderService {
                 .orElseThrow(() -> new UserNotFoundException("Cannot create cardFolder, because user not found! User id: " + data.userId()));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public Page<CardFolder> getCardFolderPageByUserId(Long userId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
@@ -47,12 +50,14 @@ public class CardFolderServiceImpl implements CardFolderService {
         return cardFolderRepository.findAllByUserId(userId, pageable);
     }
 
+    @Transactional
     @Override
     public void deleteById(Long folderId) {
         log.info("Deleting folder with id: {}", folderId);
         cardFolderRepository.deleteById(folderId);
     }
 
+    @Transactional
     @Override
     public CardFolder update(CardFolderUpdateDtoRequest request, CardFolderUpdateData data) {
         return cardFolderRepository.findById(request.getId())
@@ -64,12 +69,14 @@ public class CardFolderServiceImpl implements CardFolderService {
                 }).orElseThrow(() -> new CardFolderNotFoundException("Card folder not found! Id: " + request.getId()));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public CardFolder getById(Long folderId) {
         return cardFolderRepository.findById(folderId)
                 .orElseThrow(() -> new CardFolderNotFoundException("Card folder not found! Id: " + folderId));
     }
 
+    @Transactional(readOnly = true)
     @Override
     public boolean existsByIdAndUserId(Long folderId, Long userId) {
         return cardFolderRepository.existsByIdAndUserId(folderId, userId);
