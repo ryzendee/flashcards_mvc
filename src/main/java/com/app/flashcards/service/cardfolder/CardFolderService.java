26d303c23a2883ef2aa2869c0cacacaf290778f4
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 16:29:12 +0500

    refactor: Moved facade logic into cardfolder service, changed arguments in methods

--- src/main/java/com/app/flashcards/service/cardfolder/CardFolderService.java
+++ src/main/java/com/app/flashcards/service/cardfolder/CardFolderService.java
@@ -2,17 +2,14 @@ package com.app.flashcards.service.cardfolder;
 
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.dto.request.CardFolderUpdateDtoRequest;
-import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.models.CardFolderCreationData;
-import com.app.flashcards.models.CardFolderUpdateData;
+import com.app.flashcards.dto.response.CardFolderDtoResponse;
 import org.springframework.data.domain.Page;
 
 public interface CardFolderService {
-    CardFolder createCardFolder(CardFolderCreateDtoRequest createRequest, CardFolderCreationData creationData);
+    void createCardFolder(Long userId, CardFolderCreateDtoRequest createRequest);
 
-    Page<CardFolder> getCardFolderPageByUserId(Long userId, int page, int size);
+    Page<CardFolderDtoResponse> getCardFolderPageByUserId(Long userId, int page, int size);
     void deleteById(Long folderId);
-    CardFolder update(CardFolderUpdateDtoRequest request, CardFolderUpdateData data);
-    CardFolder getById(Long folderId);
+    void updateCardFolder(CardFolderUpdateDtoRequest request);
     boolean existsByIdAndUserId(Long folderId, Long userId);
 }
--- src/main/java/com/app/flashcards/service/cardfolder/CardFolderServiceImpl.java
+++ src/main/java/com/app/flashcards/service/cardfolder/CardFolderServiceImpl.java
@@ -2,14 +2,16 @@ package com.app.flashcards.service.cardfolder;
 
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.dto.request.CardFolderUpdateDtoRequest;
+import com.app.flashcards.dto.response.CardFolderDtoResponse;
 import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.exception.custom.CardFolderNotFoundException;
+import com.app.flashcards.enums.ImagePath;
 import com.app.flashcards.exception.custom.UserNotFoundException;
 import com.app.flashcards.factory.cardfolder.CardFolderFactory;
-import com.app.flashcards.models.CardFolderCreationData;
-import com.app.flashcards.models.CardFolderUpdateData;
+import com.app.flashcards.mapper.cardfolder.CardFolderMapper;
+import com.app.flashcards.models.ImageData;
 import com.app.flashcards.repository.CardFolderRepository;
 import com.app.flashcards.repository.UserRepository;
+import com.app.flashcards.service.image.ImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -17,6 +19,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @Slf4j
@@ -26,59 +29,72 @@ public class CardFolderServiceImpl implements CardFolderService {
     private final CardFolderRepository cardFolderRepository;
     private final UserRepository userRepository;
     private final CardFolderFactory cardFolderFactory;
-
+    private final CardFolderMapper cardFolderMapper;
+    private final ImageService imageService;
 
     @Transactional
     @Override
-    public CardFolder createCardFolder(CardFolderCreateDtoRequest createRequest, CardFolderCreationData data) {
-        return userRepository.findById(data.userId())
-                .map(user -> {
-                    CardFolder cardFolder = cardFolderFactory.createFromRequest(createRequest, data);
+    public void createCardFolder(Long userId, CardFolderCreateDtoRequest createRequest) {
+        userRepository.findById(userId)
+                .ifPresentOrElse(user -> {
+                    ImageData imageData = buildImageData(userId, createRequest.getImage());
+                    String imageUrl = imageService.uploadImage(imageData);
+
+                    CardFolder cardFolder = cardFolderFactory.createFromRequest(createRequest);
                     cardFolder.setUser(user);
+                    cardFolder.setImageUrl(imageUrl);
+
                     cardFolderRepository.save(cardFolder);
                     log.info("Card Folder was saved: {}", cardFolder);
-                    return cardFolder;
-                })
-                .orElseThrow(() -> new UserNotFoundException("Cannot create cardFolder, because user not found! User id: " + data.userId()));
+                }, () -> {
+                    throw new UserNotFoundException("Cannot create cardFolder, because user not found! User id: " + userId);
+                });
     }
+
 
     @Transactional(readOnly = true)
     @Override
-    public Page<CardFolder> getCardFolderPageByUserId(Long userId, int page, int size) {
+    public Page<CardFolderDtoResponse> getCardFolderPageByUserId(Long userId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
-
-        return cardFolderRepository.findAllByUserId(userId, pageable);
+        return cardFolderRepository.findAllByUserId(userId, pageable)
+                .map(cardFolderMapper::toDto);
     }
 
     @Transactional
     @Override
     public void deleteById(Long folderId) {
-        log.info("Deleting folder with id: {}", folderId);
-        cardFolderRepository.deleteById(folderId);
+        cardFolderRepository.findById(folderId)
+                .ifPresent(entity -> {
+                    cardFolderRepository.delete(entity);
+                    imageService.deleteImage(entity.getImageUrl());
+                    log.info("Deleted card folder: {}", entity);
+                });
     }
 
     @Transactional
     @Override
-    public CardFolder update(CardFolderUpdateDtoRequest request, CardFolderUpdateData data) {
-        return cardFolderRepository.findById(request.getId())
-                .map(entity -> {
+    public void updateCardFolder(CardFolderUpdateDtoRequest request) {
+        cardFolderRepository.findById(request.getId())
+                .ifPresent(entity -> {
+                    ImageData imageData = buildImageData(entity.getUser().getId(), request.getImage());
+                    String imageUrl = imageService.uploadImage(imageData);
+
+                    entity.setImageUrl(imageUrl);
                     entity.setName(request.getName());
                     entity.setDescription(request.getDescription());
-                    entity.setImageUrl(data.imageUrl());
-                    return cardFolderRepository.save(entity);
-                }).orElseThrow(() -> new CardFolderNotFoundException("Card folder not found! Id: " + request.getId()));
-    }
 
-    @Transactional(readOnly = true)
-    @Override
-    public CardFolder getById(Long folderId) {
-        return cardFolderRepository.findById(folderId)
-                .orElseThrow(() -> new CardFolderNotFoundException("Card folder not found! Id: " + folderId));
+                    cardFolderRepository.save(entity);
+                    log.info("Card folder was updated: {}", entity);
+                });
     }
 
     @Transactional(readOnly = true)
     @Override
     public boolean existsByIdAndUserId(Long folderId, Long userId) {
         return cardFolderRepository.existsByIdAndUserId(folderId, userId);
+    }
+
+    private ImageData buildImageData(Long userId, MultipartFile image) {
+        return new ImageData(userId, image, ImagePath.CARDFOLDER_PATH);
     }
 }
