1f74a86c50869169775cc36b14c710f07e108470
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 16:47:43 +0500

    feat: Added GenerateUrlException

---
+++ src/main/java/com/app/flashcards/exception/custom/GenerateUrlException.java
@@ -0,0 +1,8 @@
+package com.app.flashcards.exception.custom;
+
+public class GenerateUrlException extends RuntimeException {
+
+    public GenerateUrlException(String message) {
+        super(message);
+    }
+}
