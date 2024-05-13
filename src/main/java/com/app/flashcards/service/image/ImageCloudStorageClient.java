6f9d37a9bcad5fc96765e702c4a3e049011d47d5
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-13 16:54:52 +0500

    feat: Added generateUrlToImage method

--- src/main/java/com/app/flashcards/service/image/ImageCloudStorageClient.java
+++ src/main/java/com/app/flashcards/service/image/ImageCloudStorageClient.java
@@ -5,4 +5,5 @@ import com.app.flashcards.models.ImageData;
 public interface ImageCloudStorageClient {
     String uploadImage(ImageData imageData);
     boolean deleteImage(String imagePath);
+    String generateUrlToImage(String imagePath);
 }
