cf5280621ee4b1f5662245c2c9b09abfcd30d589
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-14 12:23:58 +0500

    refactor: Moved facade logic into flashcard service

--- src/main/java/com/app/flashcards/service/flashcard/FlashcardService.java
+++ src/main/java/com/app/flashcards/service/flashcard/FlashcardService.java
@@ -2,18 +2,16 @@ package com.app.flashcards.service.flashcard;
 
 import com.app.flashcards.dto.request.FlashcardCreateDtoRequest;
 import com.app.flashcards.dto.request.FlashcardUpdateDtoRequest;
-import com.app.flashcards.entity.Flashcard;
-import com.app.flashcards.models.FlashcardCreationData;
-import com.app.flashcards.models.FlashcardUpdateData;
+import com.app.flashcards.dto.response.FlashcardDtoResponse;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface FlashcardService {
 
-    List<Flashcard> getListByFolderId(Long folderId);
-    Page<Flashcard> getPageByFolderId(Long folderId, int page, int size);
+    List<FlashcardDtoResponse> getListByFolderId(Long folderId);
+    Page<FlashcardDtoResponse> getPageByFolderId(Long folderId, int page, int size);
     void deleteFlashcardById(Long flashcardId);
-    Flashcard createFlashcard(FlashcardCreateDtoRequest createDtoRequest, FlashcardCreationData creationData);
-    Flashcard updateFlashcard(FlashcardUpdateDtoRequest updateDtoRequest, FlashcardUpdateData updateData);
+    void createFlashcard(FlashcardCreateDtoRequest createDtoRequest);
+    void updateFlashcard(FlashcardUpdateDtoRequest updateDtoRequest);
 }
--- src/main/java/com/app/flashcards/service/flashcard/FlashcardServiceImpl.java
+++ src/main/java/com/app/flashcards/service/flashcard/FlashcardServiceImpl.java
@@ -2,15 +2,17 @@ package com.app.flashcards.service.flashcard;
 
 import com.app.flashcards.dto.request.FlashcardCreateDtoRequest;
 import com.app.flashcards.dto.request.FlashcardUpdateDtoRequest;
-import com.app.flashcards.entity.CardFolder;
+import com.app.flashcards.dto.response.FlashcardDtoResponse;
 import com.app.flashcards.entity.Flashcard;
+import com.app.flashcards.enums.ImagePath;
 import com.app.flashcards.exception.custom.CardFolderNotFoundException;
 import com.app.flashcards.exception.custom.FlashcardNotFoundException;
 import com.app.flashcards.factory.flashcard.FlashcardFactory;
-import com.app.flashcards.models.FlashcardCreationData;
-import com.app.flashcards.models.FlashcardUpdateData;
-import com.app.flashcards.repository.FlashcardRepository;
+import com.app.flashcards.mapper.flashcard.FlashcardMapper;
+import com.app.flashcards.models.ImageData;
 import com.app.flashcards.repository.CardFolderRepository;
+import com.app.flashcards.repository.FlashcardRepository;
+import com.app.flashcards.service.image.ImageCloudStorageClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,6 +20,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -29,50 +32,85 @@ public class FlashcardServiceImpl implements FlashcardService {
     private final FlashcardFactory flashcardFactory;
     private final FlashcardRepository flashcardRepository;
     private final CardFolderRepository cardFolderRepository;
+    private final ImageCloudStorageClient imageCloudStorageClient;
+    private final FlashcardMapper flashcardMapper;
 
     @Transactional(readOnly = true)
     @Override
-    public List<Flashcard> getListByFolderId(Long folderId) {
-        return flashcardRepository.findAllByCardFolderId(folderId);
+    public List<FlashcardDtoResponse> getListByFolderId(Long folderId) {
+        return flashcardRepository.findAllByCardFolderId(folderId).stream()
+                .map(entity -> {
+                    String imageUrl = imageCloudStorageClient.generateUrlToImage(entity.getImagePath());
+                    FlashcardDtoResponse dtoResponse = flashcardMapper.toDto(entity);
+                    dtoResponse.setImageUrl(imageUrl);
+                    return dtoResponse;
+                }).toList();
     }
 
     @Transactional(readOnly = true)
     @Override
-    public Page<Flashcard> getPageByFolderId(Long folderId, int page, int size) {
+    public Page<FlashcardDtoResponse> getPageByFolderId(Long folderId, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
 
-        return flashcardRepository.findAllByCardFolderId(folderId, pageable);
+        return flashcardRepository.findAllByCardFolderId(folderId, pageable)
+                .map(entity -> {
+                    String imageUrl = imageCloudStorageClient.generateUrlToImage(entity.getImagePath());
+                    FlashcardDtoResponse dtoResponse = flashcardMapper.toDto(entity);
+                    dtoResponse.setImageUrl(imageUrl);
+                    return dtoResponse;
+                });
     }
 
     @Transactional
     @Override
     public void deleteFlashcardById(Long flashcardId) {
-        log.info("Deleting flashcard with id: {}", flashcardId);
-        flashcardRepository.deleteById(flashcardId);
+        flashcardRepository.findById(flashcardId)
+                .ifPresent(entity -> {
+                    flashcardRepository.delete(entity);
+                    imageCloudStorageClient.deleteImage(entity.getImagePath());
+                    log.info("Deleted flashcard: {}", entity);
+                });
     }
 
     @Transactional
     @Override
-    public Flashcard createFlashcard(FlashcardCreateDtoRequest createDtoRequest, FlashcardCreationData creationData) {
-        return cardFolderRepository.findById(createDtoRequest.getFolderId())
-                .map(folder -> {
-                    Flashcard flashcard = flashcardFactory.createFromRequestAndData(createDtoRequest, creationData);
-                    flashcard.setCardFolder(folder);
-                    flashcardRepository.save(flashcard);
-                    log.info("Flashcard was saved: {}", flashcard);
-                    return flashcard;
-                }).orElseThrow(() -> new CardFolderNotFoundException("Cannot create flashcard, because card folder not found! Card Folder Id: " + createDtoRequest.getFolderId()));
+    public void createFlashcard(FlashcardCreateDtoRequest createDtoRequest) {
+        cardFolderRepository.findById(createDtoRequest.getFolderId())
+                .ifPresentOrElse(entity -> {
+                            Flashcard flashcard = flashcardFactory.createFromRequestAndData(createDtoRequest);
+                            flashcard.setCardFolder(entity);
+
+                            ImageData imageData = buildImageData(createDtoRequest.getUserId(), createDtoRequest.getImage());
+                            String imagePath = imageCloudStorageClient.uploadImage(imageData);
+                            flashcard.setImagePath(imagePath);
+
+                            flashcardRepository.save(flashcard);
+                            log.info("Flashcard was saved: {}", flashcard);
+                        }, () -> {
+                            throw new CardFolderNotFoundException("Cannot create flashcard because card folder not found! CardFolder Id: " + createDtoRequest.getFolderId());
+                        });
     }
 
     @Transactional
     @Override
-    public Flashcard updateFlashcard(FlashcardUpdateDtoRequest updateDtoRequest, FlashcardUpdateData updateData) {
-        return flashcardRepository.findById(updateDtoRequest.getId())
-                .map(entity -> {
+    public void updateFlashcard(FlashcardUpdateDtoRequest updateDtoRequest) {
+        flashcardRepository.findById(updateDtoRequest.getId())
+                .ifPresentOrElse(entity -> {
                     entity.setName(updateDtoRequest.getName());
                     entity.setDefinition(updateDtoRequest.getDefinition());
-                    entity.setImageUrl(updateData.imageUrl());
-                    return flashcardRepository.save(entity);
-                }).orElseThrow(() -> new FlashcardNotFoundException("Flashcard not found! Id: " + updateDtoRequest.getId()));
+
+                    ImageData imageData = buildImageData(updateDtoRequest.getUserId(), updateDtoRequest.getImage());
+                    String imagePath = imageCloudStorageClient.uploadImage(imageData);
+                    entity.setImagePath(imagePath);
+
+                    flashcardRepository.save(entity);
+                    log.info("Flashcard was updated: {}", entity);
+                }, () -> {
+                    throw new FlashcardNotFoundException("Flashcard not found! Id: " + updateDtoRequest.getId());
+                });
+    }
+
+    private ImageData buildImageData(Long userId, MultipartFile image) {
+        return new ImageData(userId, image, ImagePath.FLASHCARDS_PATH);
     }
 }
