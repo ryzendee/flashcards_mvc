795b704e71a9146e9eaedbe51c90fbe335a3cf53
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-14 12:51:17 +0500

    refactor: Updated factory tests

--- src/test/java/com/app/flashcards/unit/factory/CardFolderFactoryTest.java
+++ src/test/java/com/app/flashcards/unit/factory/CardFolderFactoryTest.java
@@ -2,16 +2,14 @@ package com.app.flashcards.unit.factory;
 
 import com.app.flashcards.dto.request.CardFolderCreateDtoRequest;
 import com.app.flashcards.entity.CardFolder;
-import com.app.flashcards.entity.User;
 import com.app.flashcards.factory.cardfolder.CardFolderFactory;
 import com.app.flashcards.factory.cardfolder.CardFolderFactoryImpl;
-import com.app.flashcards.models.CardFolderCreationData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CardFolderFactoryTest {
+class CardFolderFactoryTest {
 
     private CardFolderFactory cardFolderFactory;
 
@@ -22,15 +20,12 @@ public class CardFolderFactoryTest {
 
     @Test
     void test() {
-        CardFolderCreateDtoRequest request = new CardFolderCreateDtoRequest("test-name", "test-descr", null);
+        CardFolderCreateDtoRequest request = new CardFolderCreateDtoRequest(1L, "test-name", "test-descr", null);
 
-        User user = new User();
-        CardFolderCreationData creationData = new CardFolderCreationData(user, "test-imageUrl");
 
-        CardFolder createdFolder = cardFolderFactory.createFromRequest(request, creationData);
+        CardFolder createdFolder = cardFolderFactory.createFromRequest(request);
 
         assertThat(createdFolder.getName()).isEqualTo(request.getName());
         assertThat(createdFolder.getDescription()).isEqualTo(request.getDescription());
-        assertThat(createdFolder.getUser()).isEqualTo(creationData.user());
     }
 }
