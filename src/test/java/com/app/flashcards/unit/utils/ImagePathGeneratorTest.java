83afdb3a132b1f6b9cd62c112934c0f355eefec0
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-14 13:54:17 +0500

    refactor: Updated imagePathGenerator test

--- src/test/java/com/app/flashcards/unit/utils/ImagePathGeneratorTest.java
+++ src/test/java/com/app/flashcards/unit/utils/ImagePathGeneratorTest.java
@@ -1,14 +1,19 @@
 package com.app.flashcards.unit.utils;
 
 import com.app.flashcards.enums.ImagePath;
+import com.app.flashcards.models.ImageData;
 import com.app.flashcards.utils.path.ImagePathGenerator;
 import com.app.flashcards.utils.path.ImagePathGeneratorImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.web.multipart.MultipartFile;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-public class ImagePathGeneratorTest {
+class ImagePathGeneratorTest {
 
     private ImagePathGenerator imagePathGenerator;
 
@@ -21,8 +26,14 @@ public class ImagePathGeneratorTest {
     void generatePath_withFlashcardsImagePath_shouldGenerateExpectedPath() {
         String expectedPath = "user-1/flashcards/test.png";
 
-        String actualPath = imagePathGenerator.generatePath("1", "test.png", ImagePath.FLASHCARDS_PATH);
+        MultipartFile image = Mockito.mock(MultipartFile.class);
+        ImageData imageData = new ImageData(1L, image, ImagePath.FLASHCARDS_PATH);
+        when(image.getOriginalFilename())
+                .thenReturn("test.png");
 
+        String actualPath = imagePathGenerator.generatePath(imageData);
         assertThat(actualPath).isEqualTo(expectedPath);
+
+        verify(image).getOriginalFilename();
     }
 }
