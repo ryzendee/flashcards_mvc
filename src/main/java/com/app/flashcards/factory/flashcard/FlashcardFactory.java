5e5116a7de8f9828e7d38b261e0ae2cd7a9489a0
Dmitry Ryazantsev <ryazantsev_03@bk.ru>
2024-05-27 14:57:55 +0400

    fix: Fixed imports

--- src/main/java/com/app/flashcards/factory/flashcard/FlashcardFactory.java
+++ src/main/java/com/app/flashcards/factory/flashcard/FlashcardFactory.java
@@ -2,8 +2,6 @@ package com.app.flashcards.factory.flashcard;
 
 import com.app.flashcards.dto.request.FlashcardCreateDtoRequest;
 import com.app.flashcards.entity.Flashcard;
-import com.app.flashcards.models.FlashcardCreationData;
-
 public interface FlashcardFactory {
 
     Flashcard createFromDtoRequest(FlashcardCreateDtoRequest createDtoRequest);
--- src/main/java/com/app/flashcards/factory/flashcard/FlashcardFactoryImpl.java
+++ src/main/java/com/app/flashcards/factory/flashcard/FlashcardFactoryImpl.java
@@ -2,7 +2,6 @@ package com.app.flashcards.factory.flashcard;
 
 import com.app.flashcards.dto.request.FlashcardCreateDtoRequest;
 import com.app.flashcards.entity.Flashcard;
-import com.app.flashcards.models.FlashcardCreationData;
 import org.springframework.stereotype.Component;
 
 @Component
